099bd11ec05a732069353cae312e899fe80b5c12
Greg Stromire <gsmolodrone@gmail.com>
2014-10-10 23:50:43 -0700

    Initial Commit

---
+++ BikeGuider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeGuider
+//
+//  Created by Greg Stromire on 10/10/14.
+//  Copyright (c) 2014 Greg Stromire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
